29532e1c8a151b19be862b83c8f1f55a1711f14e
Muhammad Usman Raza <musmanraza2073@gmail.com>
2025-07-09 14:14:12 -0700

    Initial commit

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 const express = require("express");
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const cors = require("cors");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Stripe Backend is Running 🚀");
+  res.send("Stripe backend running 🚀");
 });
 
 app.post("/create-payment-intent", async (req, res) => {
@@ -26,5 +26,5 @@ app.post("/create-payment-intent", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
